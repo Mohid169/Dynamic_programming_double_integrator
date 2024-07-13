5bb85932e54027ea34139b7522ac2bb550acfd67
Mohid Khan <mohidkhan169@gmail.com>
2024-07-12 21:28:29 -0400

    test

--- dynamic_programming.py
+++ dynamic_programming.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-#Define double integrator system
+#Define double integrator system variables
 def double_integrator():
     A = np.array([[0 ,1], [0, 0]])
     B = np.array([0, 1])
