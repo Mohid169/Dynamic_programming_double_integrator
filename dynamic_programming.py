07c22dc148ffb3731e1265ec5095bf8605a118e4
Mohid <mohidkhan169@gmail.com>
2024-07-24 23:49:41 -0400

    dynamic programming

--- dynamic_programming.py
+++ dynamic_programming.py
@@ -56,7 +56,7 @@ for iteration in range(max_iterations):
             for action in actions:
                 action_vec = np.array([[action]])
                 # Calculate the next state using the Euler method for continuous-time dynamics
-                next_state = state + (A @ state + B @ action_vec) * dt
+                next_state = state + (A @ state + B @ action_vec) * dt # euler integration to find the next state 
                 next_state = next_state.flatten()  # Flatten for indexing
                 next_state_index = np.argmin(np.linalg.norm(np.array(states) - next_state, axis=1))
                 cost = cost_function(state, action_vec, Q, R) + J[t + 1][next_state_index]  # cost of being at the current state + Cost to go
@@ -83,56 +83,32 @@ print("Optimal policy u:", u_opt)
 
 
 #TODO: Implement opitmal control policy extraction
-'''
-psuedo code:   for iteration in range(max_iterations):
-    J_new = array of size (num_time_steps + 1), initialized to None
-    J_new[num_time_steps] = J[num_time_steps]
-    
-    for t in range(num_time_steps - 1, -1, -1):
-        J_new[t] = zero_matrix(len(states), 1)
-        
-        for i, state in enumerate(states):
-            min_cost = infinity
-            best_action = None
-            
-            for action in actions:
-                next_state = A @ state + B * action * dt
-                next_state_index = index_of_closest_state(states, next_state)
-                
-                cost = cost_function(state, action, Q, R) + J[t + 1][next_state_index]
-                
-                if cost < min_cost:
-                    min_cost = cost
-                    best_action = action
-            
-            J_new[t][i] = min_cost
-            u_opt[t] = best_action
-    
-    # Check for convergence
-    if max(abs(J_new[t] - J[t])) < tolerance for t in range(num_time_steps):
-        J = J_new
-        break
-    
-    J = J_new
+# Extract the optimal policy
+
 
 # Extract the optimal policy
-pi_star = array of size len(states), initialized to None
+pi_star = [np.zeros(len(states)) for _ in range(num_time_steps + 1)]
 
 for i, state in enumerate(states):
-    min_cost = infinity
-    best_action = None
-    
+    min_cost = float('inf')
+    best_action = None 
+
     for action in actions:
-        next_state = A @ state + B * action * dt
-        next_state_index = index_of_closest_state(states, next_state)
-        
-        cost = cost_function(state, action, Q, R) + J[0][next_state_index]
-        
-        if cost < min_cost:
-            min_cost = cost
-            best_action = action
+        next_state = state + (A @ state + B @ state )*dt 
+       # next_state =  ß
+#for i, state in enumerate(states):
+ #   min_cost = infinity
+  #  best_action = None
     
-    pi_star[i] = best_action
+    f#or action in actions:
+      #  next_state = A @ state + B * action * dt
+      #  next_state_index = index_of_closest_state(states, next_state)
+        
+      #  cost = cost_function(state, action, Q, R) + J[0][next_state_index]
+        
+       # if cost < min_cost:
+       #     min_cost = cost
+        #    best_action = action
+    
+   # pi_star[i] = best_action
 
-
-'''
